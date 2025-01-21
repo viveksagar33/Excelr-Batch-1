a426cc08ec45f17b3f7c938b5a7cd5784d9efd96
viveksagar33 <viveksagar3300@gmail.com>
2025-01-21 19:37:15 +0530

    Greatest of 3 Numbers

---
+++ demo03.java
@@ -0,0 +1,18 @@
+public class demo03 {
+    public static void main(String[] args){
+        int num1,num2,num3;
+        num1=25;
+        num2=35;
+        num3=28;
+        if (num1>num2){
+            System.out.println("num1 is greater");
+        }
+        else if(num2>num3){
+            System.out.println("num2 is greater");
+        }
+        else {
+            System.out.println("num3 if greater");
+        }
+    }
+    
+}
