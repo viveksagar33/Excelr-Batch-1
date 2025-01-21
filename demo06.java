bb22da9bc8b239baa98c5c90cbf832af0e974b8c
viveksagar33 <viveksagar3300@gmail.com>
2025-01-21 19:42:38 +0530

    User Details Using While loop

---
+++ demo06.java
@@ -0,0 +1,11 @@
+public class demo06 {
+    public static void main(String[] args) {
+        String username = "vivek";
+        String pwd = "vivek@301";
+        while((username == "vivek") && (pwd == "vivek@301")) {
+            System.out.println("Welcome to mallareddy university");
+            break;
+        }
+    }
+    
+}
