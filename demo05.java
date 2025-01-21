ebc127be9a2eee2040fdfdc278c2e0a0aa259afe
viveksagar33 <viveksagar3300@gmail.com>
2025-01-21 19:41:13 +0530

    User Credentials

---
+++ demo05.java
@@ -0,0 +1,11 @@
+public class demo05 {   
+    public static void main(String[] args) {
+        String username = "user1";
+        String pwd = "1234";
+        while((username == "user1") && (pwd == "1234")) {
+            System.out.println("hello user Happy new year");
+            break;
+        }
+    }
+    
+}
