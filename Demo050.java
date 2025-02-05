53f9c595bc9e6152b1e31c78f37b9b506278ecbd
viveksagar33 <viveksagar3300@gmail.com>
2025-02-05 10:57:52 +0530

    Multiplication Table

---
+++ Demo050.java
@@ -0,0 +1,13 @@
+
+    public class Demo050 {
+        public static void main(String[] args) {
+            int num = 5;
+            int limit = 10;
+    
+            System.out.println("Multiplication Table of " + num + ":");
+            for (int i = 1; i <= limit; i++) {
+                System.out.println(num + " * " + i + " = " + (num * i));
+            }
+        }
+    }
+
