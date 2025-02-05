8e769dbb4ca53ff47df8ce73553d11b996cea1e8
viveksagar33 <viveksagar3300@gmail.com>
2025-02-05 11:41:33 +0530

    Print Array Elements

---
+++ Demo065.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 7};
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
