414dc3b56222f559802e2e141bf8e562a09389a2
viveksagar33 <viveksagar3300@gmail.com>
2025-02-06 11:14:23 +0530

    Even Elements of an Array

---
+++ Demo067.java
@@ -0,0 +1,12 @@
+public class Demo067 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6};
+
+        System.out.println("Even elements:");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+}
