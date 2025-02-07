a650cf834baa11ef90aaa082091aba3a13d1449d
viveksagar33 <viveksagar3300@gmail.com>
2025-02-07 11:17:42 +0530

    Prime Numbers in the Array

---
+++ Demo070.java
@@ -0,0 +1,23 @@
+public class Demo070 {
+    public static void main(String[] args) {
+        int[] arr = {10, 21, 30, 11, 13, 40, 17, 19};
+
+        System.out.println("Prime numbers in the array:");
+        for (int i = 0; i < arr.length; i++) {
+            if (isPrime(arr[i])) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
